eb3042a5aca766e311660f4fdd31bcfb54e254ed
BakhtinArtem <66804919+BakhtinArtem@users.noreply.github.com>
2024-04-14 14:39:12 +0200

    update ports task-02

--- 02-soap/example/src/main/java/org/example/Runner.java
+++ 02-soap/example/src/main/java/org/example/Runner.java
@@ -7,7 +7,7 @@ import org.example.robots.RobotRequestImpl;
 public class Runner {
     public static void main(String[] args)
     {
-        Endpoint.publish("http://127.0.0.1:8000/LICENSE", new LicenseGeneratorImpl());
-        Endpoint.publish("http://127.0.0.1:8000/ROBOTS", new RobotRequestImpl());
+        Endpoint.publish("http://127.0.0.1:8001/LICENSE", new LicenseGeneratorImpl());
+        Endpoint.publish("http://127.0.0.1:8001/ROBOTS", new RobotRequestImpl());
     }
 }
